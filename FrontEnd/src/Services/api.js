26d4423179e02011e2c3164f1c5535498e8c11b7
Shilpa Patil <shilpa.patil@symphonyai.com>
2026-08-27 13:37:19 +0530

    Added cloud hosted app service URL in React

--- FrontEnd/src/Services/api.js
+++ FrontEnd/src/Services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://localhost:7151/api"
+    baseURL: "https://app-booknest-exemdtd9dzaeb2hk.centralindia-01.azurewebsites.net/api"
 });
 
 export default api;
